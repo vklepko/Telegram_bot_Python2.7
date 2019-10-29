faaec28865a8ac91a7d095930e343f0e381c278f
Yehor Koval <yekoval@playtika.com>
2019-10-30 00:01:49 +0200

    Decode text in UTF-8

--- bot.py
+++ bot.py
@@ -13,32 +13,31 @@ def get_text_messages(message):
 	if message.from_user.id in users:
 		start(message)
 	else:
-		error(message)	
+		error(message)
 
 def start(message):
 	data = message.text.split(" ", 2)
 	if len(data) != 2:
 		error(message)
 	else:
-		if data[1] == "younger":
+		if data[1] == "младший".decode('utf-8'):
 			exec_command(data[0], "blacklist", message)
-		elif data[1] == "older":
+		elif data[1] == "старший".decode('utf-8'):
 			exec_command(data[0], "blacklist1", message)
 		else:
-			error(message)	
-		
+			error(message)
+
 def exec_command(num, table_name, message):
-	MyOut = subprocess.Popen(['./block.sh', table_name, num], 
-            stdout=subprocess.PIPE, 
+	MyOut = subprocess.Popen(['./block.sh', table_name, num],
+            stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT)
 	stdout,stderr = MyOut.communicate()
 	if stdout.find("Updated") != -1:
 		bot.send_message(message.from_user.id, "Пользователь заблокирован.".decode('utf-8'))
 	else:
-		bot.send_message(message.from_user.id, "Ошибка, повторите попытку позже")
+		bot.send_message(message.from_user.id, "Ошибка, повторите попытку позже".decode('utf-8'))
 
 def error(message):
 	bot.send_message(message.from_user.id, "пошел нахуй".decode('utf-8'))
 
 bot.polling(none_stop=True, interval=0)
-
